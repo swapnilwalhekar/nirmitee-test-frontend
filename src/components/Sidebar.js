ae71aaf0361f3c1a2ef8779d50e350b16a7e3087
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2025-01-01 23:29:26 +0530

    Sidebar Updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Sidebar.css";
+import logo from "../assets/logo.jpeg";
 
 function Sidebar() {
   const location = useLocation();
 
   return (
     <div className="sidebar">
-      <h2 className="sidebar-title">Navigation</h2>
+      <h2 className="sidebar-title"></h2>
+      <img src={logo} alt="Logo" className="sidebar-logo" />
+
       <ul className="sidebar-menu">
         <li className={location.pathname === "/" ? "active" : ""}>
           <Link to="/">📅 Calendar</Link> {/* Calendar at the top */}
